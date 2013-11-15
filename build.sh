363c214bbf563bcd943330258134f61abc672548
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-11-15 21:58:56 +0900

    support patch

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ cp ../osv_hack/config.json .
 cp ../osv_hack/*.skel .
 cp ../osv_hack/build.mk .
 cp ../osv_hack/Makefile .
+curl "https://13218147686985271975.googlegroups.com/attach/803b8d581b2fd003/nbd_client.py?part=2&view=1&vt=ANaJVrFCeOz59U-oZ1gU47Hz5vz9zP1cIYzumaLChBXXL2nqR6t56jEoVuTWxj2Rrqlea6ym4ddsKcUJPK37MY2D20z4rHwYhme7nDcGKxz3zqEz9Su9K30" > scripts/nbd_client.py
 sudo make external all $*
 cd -
