47cacc46c9cc9c4c282d180e6c622a0af36a2229
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-11-16 10:23:57 +0900

    Fix path

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ cp ../osv_hack/config.json .
 cp ../osv_hack/*.skel .
 cp ../osv_hack/build.mk .
 cp ../osv_hack/Makefile .
-cp -pf nbd_client.py scripts/nbd_client.py
+cp -pf ../nbd_client.py scripts/nbd_client.py
 sudo make external all $*
 cd -
